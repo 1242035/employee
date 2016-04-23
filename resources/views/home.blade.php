9f73c05461eba0eb3eede09b90f83a8eccc5d6fb
Administrator <cam@foobla.com>
2016-04-23 21:39:01 +0700

    Fix Home Blade view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
                 <div class="col-md-4">
                     <h2>App Info</h2>
                     <p>The Employee Directory use case is simple enough to be easily understandable. This app take very much effort to complete. So I hope I like it. If there are any errors during execution, please send me a message! Thanks!<p>
-                    </p><p><a class="btn btn-default" href="#"><i class="fa fa-info-circle"></i> More Info »</a></p>
+                    </p><p><a class="btn btn-default" href="https://github.com/HoangLaravel/employee"><i class="fa fa-info-circle"></i> More Info »</a></p>
                 </div>
             </div>
         </div>
