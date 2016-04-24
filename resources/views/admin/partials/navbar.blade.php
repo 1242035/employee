fa46016334df39bbb52e7d44a6e02ff254ee397c
Administrator <cam@foobla.com>
2016-04-24 16:16:00 +0700

    Fix icon font-awesome

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -19,13 +19,13 @@
     <li><a href="/auth/login">Login</a></li>
   @else
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }}
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-user"></i> {{ Auth::user()->name }}
             <span class="caret"></span>
         </a>
         <ul class="dropdown-menu" role="menu">
-            <li><a href="/auth/adduser">Add Admin</a></li>
-            <li><a href="/auth/logout">Logout</a></li>
-            <li><a href="/change-password">Change Password</a></li>
+            <li><a href="/auth/adduser"><i class="fa fa-plus"></i> Add Admin</a></li>
+            <li><a href="/auth/logout"><i class="fa fa-sign-out"></i> Logout</a></li>
+            <li><a href="/change-password"><i class="fa fa-key"></i> Change Password</a></li>
         </ul>
     </li>
   @endif
